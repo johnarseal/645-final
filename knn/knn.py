1b03c258b88cd07976bf59dfd904b6d7967b05ce
Lynne <ldong1@andrew.cmu.edu>
2019-05-13 10:28:52 -0700

    cuda knn

---
+++ knn/knn.py
@@ -0,0 +1,83 @@
+
+import numpy as np
+import pycuda.driver as cuda
+import pycuda.autoinit
+from pycuda.compiler import SourceModule
+import os, math, sys, getopt
+import matplotlib.pyplot as plt
+K = 1
+threads_per_block = 16
+    
+def knn(traning_data, test_data): 
+    '''
+     training_data: dim(400) * train_num(1500) 
+     test_data: dim(400) *  test_num
+     train_label: 1 * 1500
+    '''
+    event = cuda.Event()
+    
+    """ Step 0 prepare data and allocate memory """
+    training_data = training_data.astype(np.float32) # ref_dev
+    traning_data_gpu = cuda.mem_alloc(traning_data.nbytes)
+    cuda.memcpy_htod(traning_data_gpu, traning_data)
+    train_dim, train_num = training_data.shape
+    print("train_dim :"+train_dim + " train_num: "+train_num)
+
+    test_data = test_data.astype(np.float32)  # query_dev
+    test_data_gpu = cuda.mem_alloc(test_data.nbytes)
+    cuda.memcpy_htod(test_data_gpu, test_data)
+    test_dim, test_num = test_data.shape
+    print("test_dim :"+test_dim + " test_num: "+test_num)
+
+    similarity = np.zeros([test_num, train_num])
+    similarity = similarity.astype(np.float32)
+    similarity_gpu = cuda.mem_alloc(similarity.nbytes)
+    cuda.memcpy_htod(similarity_gpu, similarity)
+
+    index = np.zeros([test_num, k])
+    index = index.astype(np.float32)
+    index_gpu = cuda.mem_alloc(index.nbytes)
+    cuda.memcpy_htod(index_gpu, index)
+    
+    """ Step 1. Load cuda module """
+    src = open("knnCUDA.cu").read()
+    mod = SourceModule(src, include_dirs=[os.getcwd()])
+    compute_dist_global = mod.get_function("cuComputeDistanceGlobal")
+    insertion_sort = mod.get_function("cuInsertionSort")
+    
+    """ Step 2. define some constant """
+    dim_grid = (math.ceil(float(train_num) / threads_per_block), math.ceil(float(test_num) / threads_per_block), 1)
+    dim_block = (threads_per_block, threads_per_block, 1)
+    
+    """ Step 3. Init centroids using first K elements, define some variables """
+    compute_dist_global(training_data_gpu, np.int32(train_num), test_data_gpu, 
+        np.int32(test_num), np.int32(test_dim), similarity_gpu, block = dim_block, grid = dim_grid)
+
+    event.synchronize()
+
+    insertion_sort(similarity_gpu, index_gpu, np.int32(test_num), 
+        np.int32(train_num), K, block = dim_block, grid = dim_grid)
+
+    event.synchronize()
+
+    cuda.memcpy_dtoh(similarity, similarity_gpu)
+    cuda.memcpy_dtoh(index, index_gpu)  
+    print(similarity)
+    print(index)
+
+def generate_hist(dim, num):
+    data = []
+    for i in range(num):
+        mu = 85
+        sigma = 4
+        np.random.seed(0)
+        s = np.random.normal(mu, sigma, dim)
+        plt.hist(s, 30, normed=True) 
+        data.append(s)
+
+
+if __name__ == '__main__':
+    # generate 
+    training_data = generate_hist(400, 1500)
+    test_data = generate_hist(400, 1)
+    knn(training_data, test_data)
---
+++ knn/knnCUDA.cu
@@ -0,0 +1,179 @@
+#include <cstdio>
+#include "cuda.h"
+
+// Constants used by the program
+#define BLOCK_DIM                      16
+
+/**
+  * Computes the distance between two matrix A (reference points) and
+  * B (query points) containing respectively wA and wB points.
+  *
+  * @param A     pointer on the matrix A
+  * @param wA    width of the matrix A = number of points in A
+  * @param B     pointer on the matrix B
+  * @param wB    width of the matrix B = number of points in B
+  * @param dim   dimension of points = height of matrices A and B
+  * @param AB    pointer on the matrix containing the wA*wB distances computed
+  */
+__global__ void cuComputeDistanceGlobal( float* A, int wA,
+    float* B, int wB, int dim,  float* AB){
+
+  // Declaration of the shared memory arrays As and Bs used to store the sub-matrix of A and B
+  __shared__ float shared_A[BLOCK_DIM][BLOCK_DIM];
+  __shared__ float shared_B[BLOCK_DIM][BLOCK_DIM];
+
+  // Sub-matrix of A (begin, step, end) and Sub-matrix of B (begin, step)
+  __shared__ int begin_A;
+  __shared__ int begin_B;
+  __shared__ int step_A;
+  __shared__ int step_B;
+  __shared__ int end_A;
+
+  // Thread index
+  int tx = threadIdx.x;
+  int ty = threadIdx.y;
+
+  // Other variables
+  float tmp;
+  float ssd = 0;
+
+  // Loop parameters
+  begin_A = BLOCK_DIM * blockIdx.y;
+  begin_B = BLOCK_DIM * blockIdx.x;
+  step_A  = BLOCK_DIM * wA;
+  step_B  = BLOCK_DIM * wB;
+  end_A   = begin_A + (dim-1) * wA;
+
+    // Conditions
+  int cond0 = (begin_A + tx < wA); // used to write in shared memory
+  int cond1 = (begin_B + tx < wB); // used to write in shared memory & to computations and to write in output matrix
+  int cond2 = (begin_A + ty < wA); // used to computations and to write in output matrix
+
+  // Loop over all the sub-matrices of A and B required to compute the block sub-matrix
+  for (int a = begin_A, b = begin_B; a <= end_A; a += step_A, b += step_B) {
+    // Load the matrices from device memory to shared memory; each thread loads one element of each matrix
+    if (a/wA + ty < dim){
+      shared_A[ty][tx] = (cond0)? A[a + wA * ty + tx] : 0;
+      shared_B[ty][tx] = (cond1)? B[b + wB * ty + tx] : 0;
+    }
+    else{
+      shared_A[ty][tx] = 0;
+      shared_B[ty][tx] = 0;
+    }
+
+    // Synchronize to make sure the matrices are loaded
+    __syncthreads();
+
+    // Compute the difference between the two matrixes; each thread computes one element of the block sub-matrix
+    if (cond2 && cond1){
+      for (int k = 0; k < BLOCK_DIM; ++k){
+        if (shared_A[k][ty] > shared_B[k][tx]) {
+           tmp = shared_B[k][tx];
+        } else {
+           tmp = shared_A[k][ty];
+        }
+        ssd -= tmp;
+      }
+    }
+
+    // Synchronize to make sure that the preceding computation is done before loading two new sub-matrices of A and B in the next iteration
+    __syncthreads();
+  }
+
+  // Write the block sub-matrix to device memory; each thread writes one element
+  if (cond2 && cond1)
+    AB[(begin_A + ty) * wB + begin_B + tx] = ssd;
+}
+
+/**
+  * Gathers k-th smallest distances for each column of the distance matrix in the top.
+  *
+  * @param dist        distance matrix
+  * @param ind         index matrix
+  * @param width       width of the distance matrix and of the index matrix
+  * @param height      height of the distance matrix and of the index matrix
+  * @param k           number of neighbors to consider
+  */
+__global__ void cuInsertionSort(float *dist, int *ind, int width, int height, int k){
+
+  // Variables
+  int l, i, j;
+  float *p_dist;
+  int   *p_ind;
+  float curr_dist, max_dist;
+  int   curr_row,  max_row;
+  unsigned int xIndex = blockIdx.x * blockDim.x + threadIdx.x;
+
+  if (xIndex<width){
+    // Pointer shift, initialization, and max value
+    p_dist   = dist + xIndex;
+    p_ind    = ind  + xIndex;
+    max_dist = p_dist[0];
+    p_ind[0] = 1;
+
+    // Part 1 : sort kth firt elementZ
+    for (l=1; l<k; l++){
+      curr_row  = l * width;
+      curr_dist = p_dist[curr_row];
+      if (curr_dist<max_dist){
+        i=l-1;
+        for (int a=0; a<l-1; a++){
+          if (p_dist[a*width]>curr_dist){
+            i=a;
+            break;
+          }
+        }
+        for (j=l; j>i; j--){
+          p_dist[j*width] = p_dist[(j-1)*width];
+          p_ind[j*width]   = p_ind[(j-1)*width];
+        }
+        p_dist[i*width] = curr_dist;
+        p_ind[i*width]   = l+1;
+      } else {
+        p_ind[l*width] = l+1;
+      }
+      max_dist = p_dist[curr_row];
+    }
+
+    // Part 2 : insert element in the k-th first lines
+    max_row = (k-1)*width;
+    for (l=k; l<height; l++){
+      curr_dist = p_dist[l*width];
+      if (curr_dist<max_dist){
+        i=k-1;
+        for (int a=0; a<k-1; a++){
+          if (p_dist[a*width]>curr_dist){
+            i=a;
+            break;
+          }
+        }
+        for (j=k-1; j>i; j--){
+          p_dist[j*width] = p_dist[(j-1)*width];
+          p_ind[j*width]   = p_ind[(j-1)*width];
+        }
+        p_dist[i*width] = curr_dist;
+        p_ind[i*width]   = l+1;
+        max_dist             = p_dist[max_row];
+      }
+    }
+  }
+}
+
+
+/**
+  * Computes the square root of the first line (width-th first element)
+  * of the distance matrix.
+  *
+  * @param dist    distance matrix
+  * @param width   width of the distance matrix
+  * @param k       number of neighbors to consider
+  */
+__global__ void cuParallelSqrt(float *dist, int width, int k){
+    unsigned int xIndex = blockIdx.x * blockDim.x + threadIdx.x;
+    unsigned int yIndex = blockIdx.y * blockDim.y + threadIdx.y;
+  if (xIndex<width && yIndex<k)
+    dist[yIndex*width + xIndex] = sqrt(dist[yIndex*width + xIndex]);
+}
+
+
+
